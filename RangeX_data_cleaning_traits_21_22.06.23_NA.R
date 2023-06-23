44e8343f50a9bdf1c916eed3b03a4436e1e54361
Nadi25 <131859122+Nadi25@users.noreply.github.com>
2023-06-23 16:27:30 +0200

    Add files via upload
    data cleaning

--- RangeX_data_cleaning_traits_21_22.06.23_NA.R
+++ RangeX_data_cleaning_traits_21_22.06.23_NA.R
@@ -61,12 +61,12 @@ traits_high_21 <- traits_high_21 %>%
          "position_ID_original" = "leaf_no_DEAD",
          "number_leaves_LIVE" = "tillers",
          "number_leaves_DEAD" = "base.width",
-         "tillers" = "height",
+         "number_tillers" = "height",
          "vegetative_width" = "petiole_1",
          "height_vegetative_str" = "petiole_2",
-         "petiole_length_1" = "petiole_3",
-         "petiole_length_2" = "leaf_L_1",
-         "petiole_length_3" = "leaf_L_2",
+         "petiole_length1" = "petiole_3",
+         "petiole_length2" = "leaf_L_1",
+         "petiole_length3" = "leaf_L_2",
          "leaf_length1" = "leaf_L_3",
          "leaf_length2" = "sam",
          "leaf_length3" = "date",
@@ -92,6 +92,21 @@ traits_high_21 <- traits_high_21 %>%
   add_column(site = "hi")
 traits_high_21
 
+## add column year
+traits_high_21 <- traits_high_21 %>%
+  add_column(year = 2021)
+traits_high_21
+
+colnames(traits_high_21)
+
+# delete columns ----------------------------------------------------------
+
+traits_high_21 <- traits_high_21 %>%
+  dplyr::select(-A, -B)
+
+length(traits_high_21)
+
+
 
 
 # vegetative height ------------------------------------------------------
@@ -107,10 +122,6 @@ traits_high_21
 traits_high_21$height_vegetative_str
 
 
-
-# leaf length -------------------------------------------------------------
-
-## 4e, cyncri, i4, leaf_length2 is a typo --> 171 instead of 1711
 
 
 
@@ -138,7 +149,212 @@ head(traits_low_21)
 traits_low_21 <- traits_low_21 %>% drop_na(block)
 length(traits_low_21$block) ## 600 rows
 
-## 
+ 
+
+# add columns -------------------------------------------------------------
+
+## add column with region = NOR for Norway
+traits_low_21 <- traits_low_21 %>%
+  add_column(region = "NOR")
+traits_low_21
+
+## add column with site = lo for low
+traits_low_21 <- traits_low_21 %>%
+  add_column(site = "lo")
+traits_low_21
+
+## add column year
+traits_low_21 <- traits_low_21 %>%
+  add_column(year = 2021)
+traits_low_21
+
+
+
+# rename columns ----------------------------------------------------------
+colnames(traits_low_21)
+
+traits_low_21 <- traits_low_21 %>%
+  rename("block_ID_original" = "block",
+         "plot_ID_original" = "treat",
+         "position_ID_original" = "coord",
+         "number_leaves_LIVE" = "leaf_no_LIVE",
+         "number_leaves_DEAD" = "leaf_no_DEAD",
+         "vegetative_width" = "base.width",
+         "height_vegetative_str" = "height",
+         "petiole_length1" = "petiole_1",
+         "petiole_length2" = "petiole_2",
+         "petiole_length3" = "petiole_3",
+         "leaf_length1" = "leaf_L_1",
+         "leaf_length2" = "leaf_L_2",
+         "leaf_length3" = "leaf_L_3",
+         "number_tillers" = "tillers")
+traits_low_21
+
+
+# merge data traits high with traits low ---------------------------------------
+
+# check if data frames are equal
+all.equal(traits_high_21, traits_low_21)
+
+## get column names
+dput(colnames(traits_high_21))
+dput(colnames(traits_low_21))
+
+## combine high and low site
+traits_21 <- rbind(traits_high_21, traits_low_21)
+head(traits_21)
+
+# sort after site, block, plot, position
+traits_21 <- traits_21 %>%
+  group_by(site, block_ID_original , plot_ID_original) %>%
+  arrange(block_ID_original,plot_ID_original, position_ID_original, .by_group = TRUE)
+
+summary(traits_21)
+
+# load metadata file for all countries ------------------------------------------------------
+metadata <- read.csv2("RangeX_Metadata.csv")
+head(metadata)
+dput(colnames(metadata))
+
+## filter only NOR
+metadata_NOR <- metadata %>%
+  filter(grepl('NOR', region))
+head(metadata_NOR)
+
+
+# improve traits_2021 data set --------------------------------------------
+
+# change species name in one plot -----------------------------------------
+## block 9, b, d3 = sucpra instead of leuvul
+
+traits_21[traits_21$site == "lo" & traits_21$block_ID_original == "9" 
+            & traits_21$plot_ID_original == "b" 
+            & traits_21$position_ID_original == "d3", "species"] <- "sucpra"
+
+# leaf length -------------------------------------------------------------
+## 4e, cyncri, i4, leaf_length2 is a typo --> 171 instead of 1711
+traits_21[traits_21$site == "hi" & traits_21$block_ID_original == "4" 
+          & traits_21$plot_ID_original == "e" 
+          & traits_21$position_ID_original == "i4", "leaf_length2"] <- 171
+
+
+# merge metadata with trait data 21 ------------------------------------------
+
+dput(colnames(metadata_NOR))
+dput(colnames(traits_high_21))
+
+traits_2021 <- left_join(traits_21, metadata_NOR,
+                         by = c("region", "site", "block_ID_original", "plot_ID_original",
+                                "position_ID_original", "species"))
+
+
+# reorder column names ----------------------------------------------------
+## to get it in the metadata format
+dput(colnames(traits_2021))
+
+col_order_21 <- c("region", "site", "block_ID_original", "plot_ID_original", 
+               "position_ID_original", "species", "year", "treat_warming", "treat_competition", 
+               "added_focals", "block_ID", "position_ID", "unique_plot_ID", 
+               "unique_plant_ID", "height_vegetative_str", "vegetative_width", 
+               "petiole_length1", "petiole_length2", "petiole_length3", 
+               "leaf_length1", "leaf_length2", "leaf_length3", 
+               "number_tillers", "sam", "date", "notes")
+
+traits_2021 <- traits_2021[, col_order_21]
+traits_2021
+
+
+# vegetative height -------------------------------------------------------
+
+## many values have (s) or (f) 
+traits_2021$height_vegetative_str
+
+traits_2021 <- traits_2021 %>% 
+  separate(height_vegetative_str, c("height_vegetative_str", "B", "C", "D", "E", "F"))
+
+class(traits_2021$height_vegetative_str)
+
+## replace the "broken" in plot hi, 1, b, c6, plalan
+
+traits_2021[traits_2021$site == "hi" & traits_2021$block_ID_original == "1" 
+          & traits_2021$plot_ID_original == "b" 
+          & traits_2021$position_ID_original == "c6", "height_vegetative_str"] <- NA
+
+
+## delete columns B-G
+traits_2021 <- traits_2021 %>%
+  dplyr::select(-B, -C, -D, -E, -F, -G)
+
+traits_2021$height_vegetative_str <- as.numeric(traits_2021$height_vegetative_str)
+class(traits_2021$height_vegetative_str)
+
+# date --------------------------------------------------------------------
+## change format of date
+traits_2021 <- traits_2021 %>% 
+  mutate(date = as.Date(date, "%d.%m.%y"))
+
+
+
+# data exploration --------------------------------------------------------
+
+# add column treatment ----------------------------------------------------
+## you need a column with the site + treatments: hi_warm_vege
+traits_2021_exploration <- traits_2021
+
+traits_2021_exploration$treatment <- paste(traits_2021_exploration$site, 
+                                           traits_2021_exploration$treat_warming,
+                                           traits_2021_exploration$treat_competition,
+                                           sep = "_")
+
+unique(traits_2021_exploration$treatment)
+
+
+# plot figures to explore data --------------------------------------------
+## vegetative height stretched for all species and all treatments
+ggplot(data = traits_2021_exploration, aes(species, height_vegetative_str, fill = treatment))+
+  geom_boxplot()
+
+## we only have height values for hypmac and plalan
+
+## that's not so good
+
+## leaf length
+ggplot(data = traits_2021_exploration, aes(species, leaf_length1, fill = treatment))+
+  geom_boxplot()
+
+## calculate a mean of leaf_length1, 2 and 3
+
+
+
+
+
+# load metadata for data entry Yearly demographics ----------------------------------------------------------
+
+yearly_demographics <- read.csv("RangeX_YearlyDemographics.csv")
+head(yearly_demographics)
+dput(colnames(yearly_demographics))
+
+
+# adapt traits_2022 in the format of yearly demographics ------------------
+## !! use traits_2021 !!
+
+## write mail to Evelin about
+## adding sam, petiole_length2 and 3 to the yearly demographics
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
